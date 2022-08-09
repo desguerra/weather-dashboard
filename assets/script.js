afe7cc904742200414fe491d5aeff44ab994f7d4
desguerra <ddesguerra@ucdavis.edu>
2022-08-09 12:22:28 -0700

    add card and block structure skeleton

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,28 @@
 var userFormEl = document.querySelector("#user-form"); // get <form> id
 var cityInputEl = document.querySelector("#city"); // get text <input> id
 
+
+var getCityWeather = function(city) {
+    // format OpenWeather One Call api url
+    // key here, since we won't have a backend to store it for this project
+    var apiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + ",US&limit=1&appid=" + "9686278b56b94a147e1e2facc0a2671a";
+
+    fetch(apiUrl)
+        .then(function(res) {
+            if (res.ok) {
+                displayWeatherData();
+            }
+            else {
+                console.log("error: city not found");
+            }
+        })
+        
+        .catch(function(err) {
+            console.log("unable to connect to OpenWeather One Call API");
+        });
+
+};
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
@@ -8,14 +30,18 @@ var formSubmitHandler = function(event) {
     var currentCity = cityInputEl.value.trim();
 
     if (currentCity) {
-        console.log(currentCity);
+        getCityWeather(currentCity);
 
         // reset form
         cityInputEl.value = "";
     }
     else {
-        console.log("no city found");
+        console.log("please enter a city name");
     }
 };
+
+var displayWeatherData = function() {
+    console.log("display weather data function called...")
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
